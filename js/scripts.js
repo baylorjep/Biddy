b08558934604f99650027216e71042a514dc546c
19wburnett <153020893+19wburnett@users.noreply.github.com>
2024-07-12 13:13:34 -0600

    Added graph to BusinessLandingPage

--- js/scripts.js
+++ js/scripts.js
@@ -45,4 +45,38 @@ window.addEventListener('DOMContentLoaded', event => {
         details.style.display = details.style.display === 'block' ? 'none' : 'block';
     }
 
+
+    const ctx = document.getElementById('myGraph').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Job A', 'Job B', 'Job C', 'Job D'],
+            datasets: [{
+                label: 'Bids',
+                data: [12, 19, 3, 5],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+
 });
